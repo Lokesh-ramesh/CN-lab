7ec66cd3334416144f906509c454599905c8b43d
Lokesh-ramesh <95431053+Lokesh-ramesh@users.noreply.github.com>
2023-02-02 08:08:25 +0530

    Update distancevectorrouting.cpp

--- LAB8-Distance Vector Routing/distancevectorrouting.cpp
+++ LAB8-Distance Vector Routing/distancevectorrouting.cpp
@@ -1,79 +1,44 @@
-#include <bits/stdc++.h>
-using namespace std;
-#define MAX 10
-int n;
-class router {
-char adj_new[MAX], adj_old[MAX];
-int table_new[MAX], table_old[MAX];
- public:
- router( ){
-for(int i=0;i<MAX;i++) table_old[i]=table_new[i]=99;
- }
-void copy( ){
-for(int i=0;i<n;i++) {
- adj_old[i] =adj_new[i];
- table_old[i]=table_new[i];
- }
- }
-int equal( ) {
-for(int i=0;i<n;i++)
-if(table_old[i]!=table_new[i]||adj_new[i]!=adj_old[i])return 0;
-return 1;
- }
-void input(int j) {
- cout<<"Enter 1 if the corresponding router is adjacent to router"
-<<(char)('A'+j)<<" else enter 99: "<<endl<<" ";
-for(int i=0;i<n;i++)
-if(i!=j) cout<<(char)('A'+i)<<" ";
- cout<<"\nEnter matrix:";
-for(int i=0;i<n;i++) {
-if(i==j)
- table_new[i]=0;
-else
- cin>>table_new[i];
- adj_new[i]= (char)('A'+i);
- }
- cout<<endl;
- }
-void display(){
- cout<<"\nDestination Router: ";
-for(int i=0;i<n;i++) cout<<(char)('A'+i)<<" ";
- cout<<"\nOutgoing Line: ";
-for(int i=0;i<n;i++) cout<<adj_new[i]<<" ";
- cout<<"\nHop Count: ";
-for(int i=0;i<n;i++) cout<<table_new[i]<<" ";
- }
-void build(int j) {
-for(int i=0;i<n;i++)
-for(int k=0;(i!=j)&&(k<n);k++)
-if(table_old[i]!=99)
-if((table_new[i]+table_new[k])<table_new[k]) {
- table_new[k]=table_new[i]+table_new[k];
- adj_new[k]=(char)('A'+i);
- }
- }
-} r[MAX];
-void build_table( ) {
-int i=0, j=0;
-while(i!=n) {
-for(i=j;i<n;i++) {
- r[i].copy();
- r[i].build(i);
- }
-for(i=0;i<n;i++)
-if(!r[i].equal()) {
- j=i;
-break;
- }
- }
-}
-int main() {
- cout<<"Enter the number the routers(<"<<MAX<<"): "; cin>>n;
-for(int i=0;i<n;i++) r[i].input(i);
- build_table();
-for(int i=0;i<n;i++) {
- cout<<"Router Table entries for router "<<(char)('A'+i)<<":-";
- r[i].display();
- cout<<endl<<endl;
- }
+#include<stdio.h>
+struct node
+{
+	unsigned dist[20];
+	unsigned from[20];
+}rt[10];
+int main()
+{
+	int dmat[20][20];
+	int n,i,j,k,count=0;
+	printf("\nEnter the number of nodes : ");
+	scanf("%d",&n);
+	printf("\nEnter the cost matrix :\n");
+	for(i=0;i<n;i++)
+		for(j=0;j<n;j++)
+		{	
+			scanf("%d",&dmat[i][j]);
+			dmat[i][i]=0;
+			rt[i].dist[j]=dmat[i][j];
+			rt[i].from[j]=j;
+		}
+		do
+		{	
+			count=0;
+			for(i=0;i<n;i++)
+			for(j=0;j<n;j++)
+			for(k=0;k<n;k++)
+				if(rt[i].dist[j]>dmat[i][k]+rt[k].dist[j])
+				{
+					rt[i].dist[j]=rt[i].dist[k]+rt[k].dist[j];
+					rt[i].from[j]=k;
+					count++;
+				}
+		}while(count!=0);
+		for(i=0;i<n;i++)
+		{
+			printf("\n\nState value for router %d is \n",i+1);
+			for(j=0;j<n;j++)
+			{
+				printf("\t\nnode %d via %d Distance%d",j+1,rt[i].from[j]+1,rt[i].dist[j]);
+			}
+		}	
+	printf("\n\n");
 }
